ec18a33d17687343c10f8a9ea0a06904d9374e84
agroniseni88 <46695046+agroniseni88@users.noreply.github.com>
2019-02-06 22:49:51 +0100

    This is what i learned today

---
+++ index.js
@@ -0,0 +1,37 @@
+        //
+        //this is my first Javascript ! 'Hello World is strign means there is text!
+       // console.log('Hello World');
+
+       // var name="Agron";
+        //console.log(name);
+        //cannot be a reserved keyword 
+        //should be meaningful
+        // cannot start with number (1name)
+        //cannot contain a space or hyphen(-)
+        //  are case-sensitive
+
+       // var firstname = 'Agronn';
+       // var lastname='Hyseni';
+        /*
+        var a=2;
+        var b=3;
+        var total=a+b;
+        console.log(total);
+        document.write(2*(2+3));*/
+
+       /* var name='agron';
+        var surname='iseni';
+        var comment="This is your name:";
+        var total='comment'+'full';
+
+        console.log(comment + name + surname);
+        */
+      /* var agron='strings we can chan apo ndin ajo';
+       document.write(agron.charAt(2));*/
+
+       // document.write("Hello AGRON");
+var name=window.prompt("What is your name?");
+var age=window.prompt("How old are you?");
+var phnr=window.prompt("Can you provide us with your phone number please!")
+var visit=window.prompt("Last question is: How often you visit my site per week;  write in days PLEASE:")
+document.write("Welcome " + name + " , how are you today. Do you like being "+ age + " years old. Your phone number is: " + phnr+ " So it means that you like my site because you visiting " + visit + " times per week. --BRAVOOO-- I am happy to hear that you visiting " + visit + " times per week  you WOOOOOOOON")
